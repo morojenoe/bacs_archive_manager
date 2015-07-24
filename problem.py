8de17e8b49bc3c7e1d9485c159ed581d3cb3615b
Dmitry Sutygin <DemonScorpion@yandex.ru>
2015-07-24 17:19:18 +0400

    added: problem.py

---
+++ problem.py
@@ -0,0 +1,13 @@
+class Problem:
+    def __init__(self):
+        self.tests = []
+        self.sample_tests = []
+        self.statement = None
+        self.checker = None
+        self.id = None
+        self.name = None
+        self.time_limit = None
+        self.memory_limit = None
+        self.source = None
+        self.stdin = None
+        self.stdout = None
