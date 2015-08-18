429e7c87ae09e229c5e70558160c1328e9976bcd
Dmitry Sutygin <DemonScorpion@yandex.ru>
2015-08-18 11:17:21 +0400

    updated: problem.py (some of initial values substituted onto more appropriate)

--- problem.py
+++ problem.py
@@ -1,13 +1,16 @@
+import settings
+
+
 class Problem:
     def __init__(self):
         self.tests = []
         self.sample_tests = []
-        self.statement = None
+        self.statement = "problem.pdf"
         self.checker = None
-        self.id = None
-        self.name = None
-        self.time_limit = None
-        self.memory_limit = None
-        self.source = None
+        self.id = ""
+        self.name = ""
+        self.time_limit = settings.TIME_LIMIT_BY_DEFAULT
+        self.memory_limit = settings.MEMORY_LIMIT_BY_DEFAULT
+        self.source = ""
         self.stdin = None
         self.stdout = None
