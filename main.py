85d511411dde0e5c7e3c2541b83a636456c851d1
Dmitry Sutygin <DemonScorpion@yandex.ru>
2015-11-22 10:26:00 +0400

    added: now you can download problems from usaco

--- main.py
+++ main.py
@@ -8,12 +8,25 @@ from package_manager import PackageManager
 import settings
 from tests_renamer import TestRenamer
 
-contest_managers = [KTHChallengeContestManager(
-    KTHChallengeLinkMaker(),
-    KTHChallengeProblemExtractor('kth_challenge'),
-    BaseDownloader(),
-    PackageManager(TestRenamer('.*\.in$', '.*\.ans$')),
-    settings.PATH_TO_PROBLEMS)]
+from usaco.usaco_contest_manager import UsacoContestManager
+from usaco.usaco_downloader import UsacoDownloader
+from usaco.usaco_link_maker import UsacoLinkMaker
+from usaco.usaco_problem_extractor import UsacoProblemExtractor
+
+contest_managers = [
+    KTHChallengeContestManager(
+        KTHChallengeLinkMaker(),
+        KTHChallengeProblemExtractor(),
+        BaseDownloader(),
+        PackageManager(TestRenamer("^.*\.in$", "^.*\.ans$")),
+        settings.PATH_TO_PROBLEMS),
+
+    UsacoContestManager(UsacoLinkMaker(),
+                        UsacoProblemExtractor(),
+                        UsacoDownloader(),
+                        PackageManager(TestRenamer("(^.*\.in$)|(^I\..*$)",
+                                                   "(^.*\.out$)|(^O\..*)$")),
+                        settings.PATH_TO_PROBLEMS)]
 
 
 def parse_args():
@@ -21,13 +34,24 @@ def parse_args():
     parser.add_argument('command', )
     parser.add_argument('--source', help='')
     parser.add_argument('--year', help='', type=int)
+    parser.add_argument('--month', help='')
+    parser.add_argument('--division', help='')
+    parser.add_argument('--lang', help='')
     return vars(parser.parse_args())
 
 
 def process_get_command(contest_description):
     for cm in contest_managers:
-        if cm.short_name() == contest_description['source']:
+        if cm.short_name == contest_description['source']:
             cm.download_contest(contest_description)
+
+
+def process_list_command():
+    for (index, cm) in enumerate(contest_managers, start=1):
+        print("{0}) {1} - {2} ({3})".format(index,
+                                            cm.short_name,
+                                            cm.long_name,
+                                            cm.main_link))
 
 
 def main():
@@ -39,7 +63,7 @@ def main():
         del contest_description['command']
         process_get_command(contest_description)
     elif contest_description['command'] == 'list':
-        raise NotImplementedError()
+        process_list_command()
 
 
 if __name__ == '__main__':
